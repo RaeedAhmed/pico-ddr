97536faccbbf356ccbf08e8a0329b280255433b7
Raeed Ahmed <ruahmed7868@gmail.com>
2023-07-23 22:33:07 -0600

    Single color led toggling

--- code.py
+++ code.py
@@ -1,10 +1,16 @@
 import struct
+from collections import namedtuple
 from time import sleep
 
 import board
 import digitalio
+import pwmio
 import usb_hid
 from adafruit_debouncer import Debouncer
+
+Panel = namedtuple("Panel", ("button", "led"))
+BRIGHT = 2**16 - 1
+DIM = 2**12
 
 
 def create_button(pin):
@@ -13,23 +19,32 @@ def create_button(pin):
     return Debouncer(button, interval=0.003)
 
 
-def update_state(state, button, index):
-    if button.value is False:
+def create_led(pin):
+    return pwmio.PWMOut(pin, frequency=2**16)
+
+
+def update_state(state, panel, index):
+    if panel.button.value is False:
         state |= 1 << index
+        panel.led.duty_cycle = BRIGHT
     else:
         state &= ~(1 << index)
+        panel.led.duty_cycle = DIM
     return state
 
 
 def main():
     gamepad = usb_hid.devices[0]
-    button_pins = [board.GP0, board.GP1, board.GP2, board.GP3]
-    buttons = [create_button(pin) for pin in button_pins]
+    button_pins = (board.GP0, board.GP1, board.GP2, board.GP3)
+    led_pins = (board.GP15, board.GP16, board.GP17, board.GP18)
+    buttons = (create_button(pin) for pin in button_pins)
+    leds = (create_led(pin) for pin in led_pins)
+    panels = [Panel(button, led) for button, led in zip(buttons, leds)]
     button_state = 0x00
     while True:
-        for index, button in enumerate(buttons):
-            button.update()
-            button_state = update_state(button_state, button, index)
+        for index, panel in enumerate(panels):
+            panel.button.update()
+            button_state = update_state(button_state, panel, index)
         gamepad.send_report(struct.pack("<B", button_state))
 
 
